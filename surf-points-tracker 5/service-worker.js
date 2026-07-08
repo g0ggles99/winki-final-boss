580ae294a5aafa79826024e15cb8f4e9c0f9b7af
g0ggles99 <jimtonkin@gmail.com>
2026-07-08 14:24:21 +1000

    Add files via upload

---
+++ surf-points-tracker 5/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE = 'winki-final-boss-v5';
+const FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/winki-surfer-source.png', './assets/winki-app-icon.png'];
+
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(saved => saved || fetch(event.request))));
