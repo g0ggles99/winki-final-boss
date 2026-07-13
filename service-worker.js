3f394053cad0f8c6738b5afd9571a19987de99fd
g0ggles99 <jimtonkin@gmail.com>
2026-07-14 09:30:05 +1000

    Update cached app assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'winki-final-boss-v7';
-const FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/winki-surfer-source.png', './assets/winki-app-icon.png'];
+const CACHE = 'winki-final-boss-v9';
+const FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/winki-wave-icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(saved => saved || fetch(event.request))));
