0dc1b11d1bd796b97b677ea1432cf0c88cfc3906
g0ggles99 <jimtonkin@gmail.com>
2026-07-14 10:08:26 +1000

    Improve app cache refresh behavior

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,28 @@
-const CACHE = 'winki-final-boss-v9';
+const CACHE = 'winki-final-boss-v10';
 const FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/winki-wave-icon.svg'];
 
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(saved => saved || fetch(event.request))));
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(saved => saved || caches.match('./index.html')))
+  );
+});
